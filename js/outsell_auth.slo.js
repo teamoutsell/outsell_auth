ed0984133938103337de07a90f8a31d6ab243a83
Sam Elayyoub <melayyoub@outsellinc.com>
2018-07-18 22:24:46 -0700

    Update outsell_auth.slo.js

--- js/outsell_auth.slo.js
+++ js/outsell_auth.slo.js
@@ -2,7 +2,7 @@
 
   /**
    * Attach the Auth0 Lock widget to the login form.
-   */
+   
   Drupal.behaviors.outsellAuthSingleLogOff = {
     attach: function (context, settings) {
   
@@ -10,14 +10,14 @@
       var AUTH0_DOMAIN = settings.outsell_auth.domain;
       var AUTH0_CLIENT_ID = settings.outsell_auth.client_id;
       var AUTH0_RETURN_TO = settings.outsell_auth.return_to;
-  /**
+  
       // Instantiate an auth0 client
       var auth0 = new Auth0({
         domain: AUTH0_DOMAIN,
         clientID: AUTH0_CLIENT_ID,
         responseType: 'code'
       });
-    */
+    
       // Logout of Auth0 is SSO is set to false and then
       // @todo: Auth0 logout might be redundant? Might be ok to just redirect directly to user/logout
       auth0.getSSOData(true, function (err, ssoData) {
@@ -28,4 +28,5 @@
 
     }
   }
+  */
 })(jQuery);
